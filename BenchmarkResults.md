0d2d9a704946dd6169aabf8c12a3a37844b14758
Marcus Carey <mcarey@firebend.com>
2022-04-20 15:25:33 -0500

    chore: Use string in FP example to more closely match ResultInstances setup

--- BenchmarkResults.md
+++ BenchmarkResults.md
@@ -44,16 +44,18 @@ Intel Core i9-9880H CPU 2.30GHz, 1 CPU, 16 logical and 8 physical cores
 ### WithFpLanguageExt
 [Code](https://github.com/mcarey1590/fp_playground/blob/main/WithFpLanguageExt.cs)
 
-``` ini  
-  
-BenchmarkDotNet=v0.13.1, OS=macOS Monterey 12.1 (21C52) [Darwin 21.2.0]  
-Intel Core i9-9880H CPU 2.30GHz, 1 CPU, 16 logical and 8 physical cores  
-.NET SDK=6.0.101  
- [Host]     : .NET 6.0.1 (6.0.121.56705), X64 RyuJIT DefaultJob : .NET 6.0.1 (6.0.121.56705), X64 RyuJIT  
-  
-```  
-|                                           Method |     Mean |   Error |  StdDev |  Gen 0 | Allocated |  
-|------------------------------------------------- |---------:|--------:|--------:|-------:|----------:|  
-|  DoSomethingWithId_EmptyGuid_ReturnsErrorMessage | 116.4 ns | 0.43 ns | 0.36 ns | 0.0544 |     456 B |  
-| DoSomethingWithId_RandomGuid_ReturnsErrorMessage | 387.8 ns | 7.65 ns | 8.18 ns | 0.0715 |     600 B |  
-|       DoSomethingWithId_TheOne_ReturnsSuccessful | 281.3 ns | 0.93 ns | 0.87 ns | 0.0477 |     400 B |
+``` ini
+
+BenchmarkDotNet=v0.13.1, OS=macOS Monterey 12.1 (21C52) [Darwin 21.2.0]
+Intel Core i9-9880H CPU 2.30GHz, 1 CPU, 16 logical and 8 physical cores
+.NET SDK=6.0.101
+  [Host]     : .NET 6.0.1 (6.0.121.56705), X64 RyuJIT
+  DefaultJob : .NET 6.0.1 (6.0.121.56705), X64 RyuJIT
+
+
+```
+|                                           Method |      Mean |    Error |   StdDev |  Gen 0 | Allocated |
+|------------------------------------------------- |----------:|---------:|---------:|-------:|----------:|
+|  DoSomethingWithId_EmptyGuid_ReturnsErrorMessage |  29.72 ns | 0.243 ns | 0.215 ns | 0.0134 |     112 B |
+| DoSomethingWithId_RandomGuid_ReturnsErrorMessage | 227.86 ns | 3.421 ns | 3.200 ns | 0.0372 |     312 B |
+|       DoSomethingWithId_TheOne_ReturnsSuccessful | 164.27 ns | 0.696 ns | 0.617 ns | 0.0420 |     352 B |
--- WithFpLanguageExt.cs
+++ WithFpLanguageExt.cs
@@ -1,6 +1,5 @@
 using BenchmarkDotNet.Attributes;
 using LanguageExt;
-using LanguageExt.Common;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using static LanguageExt.Prelude;
 
@@ -15,38 +14,38 @@ public class WithFpLanguageExt
             .Bind(DoSomethingWithEntity)
             .Match(
                 Succ: Result<SampleClass>.Success,
-                Fail: errors => Result<SampleClass>.Error(errors.First().Message)
+                Fail: errors => Result<SampleClass>.Error(errors.First())
             );
     }
 
-    private Validation<Error, SampleClass> DoSomethingWithEntity(SampleClass model)
+    private Validation<string, SampleClass> DoSomethingWithEntity(SampleClass model)
     {
         return DoSubRoutine(model);
     }
 
-    private Validation<Error, SampleClass> DoSubRoutine(SampleClass model)
+    private Validation<string, SampleClass> DoSubRoutine(SampleClass model)
     {
         return DoSubSubRoutine1(model)
             .Bind(DoSubSubRoutine2);
     }
 
-    private Validation<Error, SampleClass> DoSubSubRoutine1(SampleClass model)
+    private Validation<string, SampleClass> DoSubSubRoutine1(SampleClass model)
     {
         // do something here
         return model;
     }
 
-    private Validation<Error, SampleClass> DoSubSubRoutine2(SampleClass model)
+    private Validation<string, SampleClass> DoSubSubRoutine2(SampleClass model)
     {
         return DoSubSubSubRoutine(model).Bind((result) =>
         {
             if (!string.IsNullOrEmpty(result.Name)) return result;
-            return Fail<Error, SampleClass>(Error.New("Does not have a name!"));
+            return Fail<string, SampleClass>("Does not have a name!");
 
         });
     }
 
-    private Validation<Error, SampleClass> DoSubSubSubRoutine(SampleClass model)
+    private Validation<string, SampleClass> DoSubSubSubRoutine(SampleClass model)
     {
         var returnVal = new SampleClass();
         if (model.Id == Guid.Parse("11111111-1111-1111-1111-111111111111"))
@@ -57,16 +56,16 @@ public class WithFpLanguageExt
         return returnVal;
     }
 
-    private Validation<Error, SampleClass> FetchEntityById(Guid id)
+    private Validation<string, SampleClass> FetchEntityById(Guid id)
     {
         return new SampleClass {Id = id};
     }
 
-    private Validation<Error, Guid> ValidateId(Guid id)
+    private Validation<string, Guid> ValidateId(Guid id)
     {
         if (id == Guid.Empty)
         {
-            return Fail<Error, Guid>(Error.New("Must provide a valid Guid Id!"));
+            return Fail<string, Guid>("Must provide a valid Guid Id!");
         }
 
         return id;
